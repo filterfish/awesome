7795ea183ff716ce9671caedfde1fc7ad3555f54
Richard Heycock <rgh@roughage.com.au>
2009-06-29 17:05:32 +1000

    Add calander to time.

--- rc.lua
+++ rc.lua
@@ -91,7 +91,7 @@ end
 
 -- {{{ Wibox
 -- Create a textbox widget
-mytextbox = widget({ type = "textbox", align = "right" })
+clockbox = widget({ type = "textbox", align = "right" })
 -- Set the default text in textbox
 -- mytextbox.text = "<b><small> " .. awesome.release .. " </small></b>"
 
@@ -180,7 +180,7 @@ for s = 1, screen.count() do
                            mytaglist[s],
                            mytasklist[s],
                            mypromptbox[s],
-                           mytextbox,
+                           clockbox,
                            mylayoutbox[s],
                            s == 1 and mysystray or nil }
     mywibox[s].screen = s
@@ -442,6 +442,48 @@ end)
 
 -- Hook called every minute
 awful.hooks.timer.register(1, function ()
-    mytextbox.text = " " .. os.date("%H:%M") .. " "
+    clockbox.text = " " .. os.date("%H:%M") .. " "
 end)
 -- }}}
+
+local calendar = nil
+local offset = 0
+
+function remove_calendar()
+    if calendar ~= nil then
+        naughty.destroy(calendar)
+        calendar = nil
+        offset = 0
+    end
+end
+
+function add_calendar(inc_offset)
+    local save_offset = offset
+    remove_calendar()
+    offset = save_offset + inc_offset
+    local datespec = os.date("*t")
+    datespec = datespec.year * 12 + datespec.month - 1 + offset
+    datespec = (datespec % 12 + 1) .. " " .. math.floor(datespec / 12)
+    local cal = awful.util.pread("cal -m " .. datespec)
+    cal = string.gsub(cal, "^%s*(.-)%s*$", "%1")
+    calendar = naughty.notify({
+        text = string.format('<span font_desc="%s">%s</span>', "monospace", os.date("%a, %d %B %Y") .. "\n" .. cal),
+        timeout = 0, hover_timeout = 0.5,
+        width = 160,
+    })
+end
+
+-- change clockbox for your clock widget (e.g. mytextbox)
+clockbox.mouse_enter = function()
+    add_calendar(0)
+end
+clockbox.mouse_leave = remove_calendar
+
+clockbox:buttons({
+    button({ }, 4, function()
+        add_calendar(-1)
+    end),
+    button({ }, 5, function()
+        add_calendar(1)
+    end),
+})
