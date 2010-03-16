8057401edc76451ff3fb6ad5cd6888cbbcfcf113
Richard Heycock <rgh@roughage.com.au>
2010-03-16 11:16:03 +1100

    Add battery monitor

--- rc.lua
+++ rc.lua
@@ -101,13 +101,50 @@ mytaglist.buttons = awful.util.table.join(
                     awful.button({ }, 5, awful.tag.viewprev)
                     )
 mytasklist = {}
+
+
+mybattmon = widget({ type = "textbox", name = "mybattmon", align = "right" })
+function battery_status ()
+    local output={} --output buffer
+    local fd=io.popen("acpitool -b", "r") --list present batteries
+    local line=fd:read()
+    while line do --there might be several batteries.
+        local battery_num = string.match(line, "Battery \#(%d+)")
+        local battery_load = string.match(line, " (%d*\.%d+)%%")
+        local time_rem = string.match(line, "(%d+\:%d+)\:%d+")
+    local discharging
+    if string.match(line, "discharging")=="discharging" then --discharging: always red
+        discharging="<span color=\"#CC7777\">"
+    elseif tonumber(battery_load)>85 then --almost charged
+        discharging="<span color=\"#77CC77\">"
+    else --charging
+        discharging="<span color=\"#CCCC77\">"
+    end
+        table.insert(output, discharging..battery_load.."%".."</span>")
+--        if battery_num and battery_load and time_rem then
+--            table.insert(output,discharging.."BAT#"..battery_num.." "..battery_load.."%% "..time_rem.."</span>")
+--        elseif battery_num and battery_load then --remaining time unavailable
+--            table.insert(output,discharging.."BAT#"..battery_num.." "..battery_load.."%%</span>")
+--        end --even more data unavailable: we might be getting an unexpected output format, so let's just skip this line.
+        line=fd:read() --read next line
+    end
+    return table.concat(output," ") --FIXME: better separation for several batteries. maybe a pipe?
+end
+mybattmon.text = " " .. battery_status() .. " "
+my_battmon_timer=timer({timeout=30})
+my_battmon_timer:add_signal("timeout", function()
+    --mytextbox.text = " " .. os.date() .. " "
+    mybattmon.text = " " .. battery_status() .. " "
+end)
+my_battmon_timer:start()
+
+
 mytasklist.buttons = awful.util.table.join(
                      awful.button({ }, 1, function (c)
                                               if not c:isvisible() then
                                                   awful.tag.viewonly(c:tags()[1])
                                               end
                                               client.focus = c
-                                              c:raise()
                                           end),
                      awful.button({ }, 3, function ()
                                               if instance then
@@ -162,6 +199,7 @@ for s = 1, screen.count() do
         mylayoutbox[s],
         s == 1 and mysystray or nil ,
         clockbox,
+        mybattmon,
         mytasklist[s],
         layout = awful.widget.layout.horizontal.rightleft
     }
